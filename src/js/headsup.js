d3376f61810aa82f4a102bdcef14bc423d605be7
pcasciola <pcasciola@gmail.com>
2018-10-31 23:26:39 -0400

    Mostly updated sounds, 10 second counter instead of playing count multiple times, Restart button hidden until cards shown

--- src/js/headsup.js
+++ src/js/headsup.js
@@ -17,16 +17,15 @@ var deviceOrientationStatus = DEVICE_ORIENTATION_UNKNOWN;
 var curPage = "";
 var curWordSet = 0;
 var curWord = 0;
-var audioLag = 500;
+var audioLag = 300;
 var startTime = 0;
 var answers = [];
 var lastTimeLeft;
 var timerInterval;
+var timeAllotted = 60;
+var lastSound = "";
+var inCountdown;
 
-/*
-
-
- */
 
 var gamewords = [
     ["Urgency", "Habit", "Social Norming","ACC ECDP", "Bystander"],
@@ -36,6 +35,7 @@ var gamewords = [
 ];
 
 var handleOrientationEvent = function(alpha, beta, gamma) {
+
     if (Math.abs(beta) < 10 || Math.abs(beta) > 170) {
         if (Math.abs(gamma) > 70)
             deviceOrientationStatus = DEVICE_ORIENTATION_READY;
@@ -44,15 +44,20 @@ var handleOrientationEvent = function(alpha, beta, gamma) {
         else if (gamma > 0 && gamma < 50)
             deviceOrientationStatus = DEVICE_ORIENTATION_UP;
     }
+
 };
 
 $(document).ready(function() {
-    console.log("ready");
-    $("body").on("touchend", function() {
-        $('body').off("touchend");
+    $("body").one("touchend", function() {
         $('body').removeClass("bodySplash");
         showPage('menu');
     });
+
+    /*
+    lowLag.init({"urlPrefix":"audio/"}); //,"force":"audioTag"
+    lowLag.load(["headsup-gameover.mp3","headsup-gameover.ogg"],"gameover");
+    lowLag.play("gameover");
+    */
 });
 
 
@@ -76,26 +81,73 @@ function playGame(wordSet) {
     });
 }
 
+function updateGetReady(n) {
+    $("#word").text(n > 3 ? "GET READY!" : n);
+    $("#word").animate({"font-size":"8rem"}, 200);
+    $("#word").animate({"font-size":"6rem"}, 400);
+    setTimeout(function () {
+        getReady(n - 1)
+    }, n > 3 ? 2000 - audioLag : 1000);
+}
+
 function getReady(n) {
     if (n > 3)
         loadSound("getready");
-    else if (n > 0)
-        loadSound("count");
+    else if (n > 2)
+        loadSound("10-seconds");
     else if (n == 0)
         loadSound("start");
 
-    if (n > 0) {
+    if (n > 2) {
         playSound(function () {
-            $("#word").text(n > 3 ? "GET READY!" : n);
-            $("#word").animate({"font-size":"8rem"}, 200);
-            $("#word").animate({"font-size":"6rem"}, 400);
-            setTimeout(function () {
-                getReady(n - 1)
-            }, n > 3 ? 2000 - audioLag : 1000 - audioLag);
+            updateGetReady(n);
         });
+    } else if (n > 0) {
+        updateGetReady(n);
     }
-    else
+    else if (n == 0)
         playSound(startGame);
+}
+
+var gestureInterval = null;
+
+function startWaitGesture(n, func1, func2) {
+    $("body").on("keydown", function(e) {
+        triggerKeyEvent(e.keyCode);
+    });
+
+    var interval = 200;
+
+    if (n == DEVICE_ORIENTATION_READY) {
+        gestureInterval = setInterval(function() {
+            if (deviceOrientationStatus == DEVICE_ORIENTATION_READY) {
+                clearWaitGesture();
+                func1();
+            }
+        }, interval);
+    } else {
+        gestureInterval = setInterval(function() {
+            if (deviceOrientationStatus == DEVICE_ORIENTATION_UP) {
+                clearWaitGesture();
+                func1();
+            } else if (deviceOrientationStatus == DEVICE_ORIENTATION_DOWN) {
+                clearWaitGesture();
+                func2();
+            }
+        }, interval);
+    }
+}
+
+function clearWaitGesture() {
+    $("body").off("keydown");
+    if (gestureInterval != null)
+        clearInterval(gestureInterval);
+}
+
+function startGameWaitGestures() {
+    startWaitGesture(DEVICE_ORIENTATION_READY, function() {
+        startWaitGesture(DEVICE_ORIENTATION_UP, function() { correct() }, function() { pass() });
+    });
 }
 
 function startGame() {
@@ -103,10 +155,8 @@ function startGame() {
     $("#word").text(gamewords[curWordSet][curWord]);
     $("#timerDiv").show();
     startTime = Date.now();
-    document.body.onkeydown = function(e) {
-        triggerKeyEvent(e.keyCode);
-    }
-    timerInterval = setInterval(updateCounter, 500);
+    timerInterval = setInterval(updateCounter, 50);
+    startGameWaitGestures();
 }
 
 function nextQuestion()
@@ -118,19 +168,23 @@ function nextQuestion()
 
         loadSound("start");
         playSound(function () {
+            if (inCountdown) startCountdown();
         });
 
         $("#page_game").removeClass("contentWrapCorrect").removeClass("contentWrapPass").addClass("contentWrap");
         $("#word").text(gamewords[curWordSet][answers.length]);
 
-        document.body.onkeydown = function (e) {
-            triggerKeyEvent(e.keyCode);
-        }
+        startGameWaitGestures();
     }
 }
 
 function gameOver() {
-    document.body.onkeydown = null;
+    /*
+    if (countdownInterval != null)
+        clearInterval(countdownInterval);
+    */
+
+    clearWaitGesture();
     clearInterval(timerInterval);
 
     $("#page_game").removeClass("contentWrapCorrect").removeClass("contentWrapPass").removeClass("contentWrap").addClass("contentWrapGameOver");
@@ -172,7 +226,7 @@ function showScore()
 
     showPage("score");
 
-    for (var x = 0 ; x < gamewords[curWordSet].length ; x++)
+    for (var x = 0 ; x < gamewords[curWordSet].length && x < answers.length; x++)
     {
         var delay = (x + 1) * 1000;
 
@@ -181,14 +235,15 @@ function showScore()
 
         setTimeout(addScoreWord, delay, x, correct);
     }
+
+    var delay = (x + 1) * 1000;
+    setTimeout(function() { $("#restartWrap").show();}, delay)
 }
-
-
 
 function triggerKeyEvent(key) {
     if (key != 38 && key != 40) return;
 
-    document.body.onkeydown = null;
+    clearWaitGesture();
 
     if (key == 38) {    //up arrow
         correct();
@@ -199,11 +254,11 @@ function triggerKeyEvent(key) {
 
 function correct() {
     answers.push(true);
-    $("#page_game").removeClass("contentWrap").removeClass("contentWrapPass").addClass("contentWrapCorrect");
     loadSound("correct");
     playSound(function() {
         setTimeout(nextQuestion, 1000);
     });
+    $("#page_game").removeClass("contentWrap").removeClass("contentWrapPass").addClass("contentWrapCorrect");
 }
 
 function pass() {
@@ -216,22 +271,45 @@ function pass() {
     });
 }
 
+function startCountdown() {
+    loadSound("10-seconds");
+    playSound(function(){});
+}
 function updateCounter() {
-    var timeLeft = 60 - Math.round((Date.now() - startTime) / 1000.0);
-    if (timeLeft < 60 && timeLeft >= 0) {
+
+    var millis = Date.now();
+
+    var timeLeft = timeAllotted - Math.round((millis - startTime) / 1000.0);
+
+    if (timeLeft != lastTimeLeft && timeLeft < timeAllotted && timeLeft >= 0) {
         var timeString = "0:" + (timeLeft < 10 ? "0" : "") + timeLeft;
         $("#timerText").text(timeString);
     }
 
-    if (timeLeft <= 10 && timeLeft > 0 && timeLeft != lastTimeLeft) {
+    millis = millis % 1000;
+
+    if (!inCountdown && timeLeft <= 11 && millis >= 0 && millis <= 200) {
+        inCountdown = true;
+        startCountdown();
+    }
+
+    /*
+    if (timeLeft <= 10 && timeLeft > 0 && timeLeft != lastTimeLeft  && (timeLeft % 1000) < audioLag && timeLeft % 2 == 0) {
+        lastTimeLeft = timeLeft;
         loadSound("count");
         playSound(function(){});
     }
+    */
+
+    if (timeLeft == 0 && timeLeft != lastTimeLeft) {
+        //Add current word to list as passed(false)
+        if (curWord < gamewords[curWordSet].length) {
+            answers.push(false);
+        }
+        gameOver();
+    }
 
     lastTimeLeft = timeLeft;
-
-    if (timeLeft <= 0)
-        gameOver();
 }
 
 function loadSound(sound) {
@@ -239,9 +317,10 @@ function loadSound(sound) {
     player.src="audio/headsup-" + sound + ".mp3";
     player.pause();
     player.load();
+    lastSound = sound;
 }
 
-function playSound(func) {
+function playSound(func, callbackOnly) {
     /*
      var player = $("#audio_" + sound);
      player.prop("currentTime", 0);
@@ -249,16 +328,26 @@ function playSound(func) {
      */
     var player = document.getElementById("audioPlayer");
 
-    //If the caller passes in a callback, play the sound and then call the callback after global audioLag milliseconds
     if (func !== undefined && func != null) {
-        player.oncanplay = function () {
-            setTimeout(function () {
-                player.play();
-                func()
-            }, audioLag);
+        if (callbackOnly !== undefined && callbackOnly) {
+            setTimeout(func, audioLag);
+        } else {
+            player.oncanplay = function () {
+                player.oncanplay = null;
+                setTimeout(function () {
+                    player.play();
+                    func();
+                }, audioLag);
+            }
         }
     }
-    else
-        player.onloadeddata = function() { setTimeout(function() { player.play() }, audioLag)};
+    else {
+        player.oncanplay = function () {
+            setTimeout(function () {
+                player.play()
+            }, audioLag)
+        };
+    }
+
 }
 
